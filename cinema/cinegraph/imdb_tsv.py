9c58933f271bcdd5b52a36862327bff51b3a1d93
Reuben Brasher <rkbrasher@gmail.com>
2021-01-29 23:48:19 -0800

    Construct a professional graph of movies

---
+++ cinema/cinegraph/imdb_tsv.py
@@ -0,0 +1,99 @@
+import pandas as pd
+
+from cinema import directories
+from cinema.cinegraph.grapher import PersonNode, WorkNode
+
+
+class IMDbPersonNode(PersonNode):
+    def __init__(self, nconst):
+        PersonNode.__init__(self, int(nconst[2:]))
+
+
+class IMDbWorkNode(WorkNode):
+    def __init__(self, tconst):
+        WorkNode.__init__(self, int(tconst[2:]))
+
+
+def load_names():
+    return pd.read_csv(
+        directories.data("name.basics.tsv.gz"), compression="gzip", sep="\t"
+    )
+
+
+def load_basics():
+    return pd.read_csv(
+        directories.data("title.basics.tsv.gz"),
+        compression="gzip",
+        sep="\t",
+        low_memory=False,
+    )
+
+
+def load_ratings():
+    return pd.read_csv(
+        directories.data("title.ratings.tsv.gz"), compression="gzip", sep="\t"
+    )
+
+
+def load_principals():
+    return pd.read_csv(
+        directories.data("title.principals.tsv.gz"), compression="gzip", sep="\t"
+    )
+
+
+def addPerson(g, row):
+    person = IMDbPersonNode(row["nconst"])
+    try:
+        known = {IMDbWorkNode(tt) for tt in row["knownForTitles"].split(",")}
+    except ValueError as err:
+        known = {}
+    try:
+        professions = set(row["primaryProfession"].split(","))
+    except AttributeError as err:
+        professions = {}
+    g.add_node(
+        person,
+        name=row["primaryName"],
+        birth=row["birthYear"],
+        death=row["deathYear"],
+        professions=professions,
+        known=known,
+    )
+
+
+def addWork(g, row):
+    work = IMDbWorkNode(row["tconst"])
+    try:
+        genres = set(row["genres"].split(","))
+    except AttributeError as err:
+        genres = {}
+    g.add_node(
+        work,
+        title=row["primaryTitle"],
+        adult=row["isAdult"],
+        start=row["startYear"],
+        end=row["endYear"],
+        runtime=row["runtimeMinutes"],
+        genres=genres,
+    )
+
+
+def updateRating(g, row):
+    work = IMDbWorkNode(row["tconst"])
+    if work not in g:
+        return
+    update = {"rating": row["averageRating"], "votes": row["numVotes"]}
+    g.nodes[work].update(update)
+
+
+def addContribution(g, row):
+    person = IMDbPersonNode(row["nconst"])
+    work = IMDbWorkNode(row["tconst"])
+    if not (person in g.nodes and work in g.nodes):
+        return
+    contribution = {"ordering": row["ordering"], "job": row["job"]}
+    contributions = {row["category"]: contribution}
+    if (person, work) in g.edges:
+        g.edges[(person, work)]["contributions"].update(contributions)
+    else:
+        g.add_edge(person, work, contributions=contributions)
--- cinema/core/scripts/make_imdb_professional_graph.py
+++ cinema/core/scripts/make_imdb_professional_graph.py
@@ -6,8 +6,12 @@
 import os
 from tqdm import tqdm
 import requests
+import networkx as nx
+import pickle
 
 from cinema import directories
+from cinema.cinegraph import imdb_tsv
+from cinema.cinegraph.grapher import PersonNode
 
 
 def retrieve_imdb_data(filename):
@@ -29,9 +33,75 @@ def retrieve_imdb_data(filename):
     progress_bar.close()
 
 
+def filteredGraphUpdate(g, df, rowUpdate, predicate=None):
+    if predicate is None:
+        predicate = lambda row: True
+    with tqdm(total=len(df)) as pbar:
+        for _, row in df.iterrows():
+            if predicate(row):
+                rowUpdate(g, row)
+            pbar.update(1)
+
+
+def populatePeople(g):
+    relevant = set("actor,actress,writer,director,producer".split(","))
+
+    def predicate(row):
+        primaryProfessions = row["primaryProfession"]
+        try:
+            professions = set(primaryProfessions.split(","))
+        except:
+            return False
+        return not relevant.isdisjoint(professions)
+
+    print("loading people data")
+    df = imdb_tsv.load_names()
+    print("data loaded")
+    filteredGraphUpdate(g, df, imdb_tsv.addPerson, predicate=predicate)
+
+
+def populateMovies(g):
+    print("loading work data")
+    df = imdb_tsv.load_basics()
+    print("data loaded")
+    predicate = lambda row: row["titleType"] == "movie"
+    filteredGraphUpdate(g, df, imdb_tsv.addWork, predicate=predicate)
+
+
+def updateRatings(g):
+    print("loading rating data")
+    df = imdb_tsv.load_ratings()
+    print("data loaded")
+    filteredGraphUpdate(g, df, imdb_tsv.updateRating)
+
+
+def makeEdges(g):
+    print("loading credits data")
+    df = imdb_tsv.load_principals()
+    print("data loaded")
+    relevantJobs = "actor,actress,writer,director,producer".split(",")
+    predicate = lambda row: row["category"] in relevantJobs
+    filteredGraphUpdate(g, df, imdb_tsv.addContribution, predicate=predicate)
+
+
+def makeProfessionalGraph():
+    g = nx.Graph()
+    populatePeople(g)
+    populateMovies(g)
+    updateRatings(g)
+    makeEdges(g)
+    # we want the component with Fred Astaire who has ID 1
+    return g.subgraph(nx.node_connected_component(g, PersonNode(1))).copy()
+
+
 def run():
-    print("hi")
+    print("Downloading data")
     retrieve_imdb_data("name.basics.tsv.gz")
     retrieve_imdb_data("title.basics.tsv.gz")
     retrieve_imdb_data("title.ratings.tsv.gz")
     retrieve_imdb_data("title.principals.tsv.gz")
+
+    print("Creating graph")
+    g = makeProfessionalGraph()
+    with open(directories.data("professional.pkl"), "wb") as f:
+        pickle.dump(g, f)
